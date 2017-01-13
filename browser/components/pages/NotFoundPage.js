5dc74f4329f86f1204e9abd31d71a3b713515388
anasauce <anasauceda1843@gmail.com>
2017-01-13 15:59:14 -0800

    made 404 page more useful/prettier

--- browser/components/pages/NotFoundPage.js
+++ browser/components/pages/NotFoundPage.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react'
+iimport React, { Component } from 'react'
+import sadCat from '../../images/SadCat.gif'
 import Link from '../atoms/Link'
+import './NotFoundPage.sass'
 
 export default class NotFoundPage extends Component {
+
   render(){
     return <div className="NotFoundPage">
-      <h1>Page Not Found</h1>
-      <h2>{this.props.location.pathname}</h2>
-      <Link href="/">Homepage</Link>
+      <div className="PageMain">
+        <h1 className="NotFound">Page Not Found</h1>
+        <h2>{this.props.location.pathname}</h2>
+        <img src={sadCat} className='SadCat'/>
+      </div>
+      <div className="links">
+        <Link className="hompageLink" href="/">Get me out of here!</Link>
+        <div className="SendIssue">
+          <h4 className="SomethingWrong">Is there something wrong?</h4>
+          <Link className="hompageLink" href="https://github.com/GuildCrafts/prrr/issues/new">Let us know!</Link>
+        </div>
+      </div>
     </div>
   }
 }
