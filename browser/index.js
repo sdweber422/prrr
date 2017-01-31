29aa7e3e4506443944a7fd5b3283e54a7ea355af
Jared Grippe <jared@deadlyicon.com>
2017-01-31 10:31:03 -0800

    added DEBUG.prrrs to help debug the prrrs cached in the browser

--- browser/index.js
+++ browser/index.js
@@ -16,9 +16,14 @@ import state from './state'
 import moment from 'moment'
 import request from './request'
 import logger from './logger'
+import Prrrs from './Prrrs'
 window.DEBUG = window.DEBUG || {}
 window.DEBUG.state = state
 window.DEBUG.React = React
 window.DEBUG.moment = moment
 window.DEBUG.request = request
 window.DEBUG.logger = logger
+window.DEBUG.prrrs = _ => {
+  const {session, prrrs} = state.get()
+  return new Prrrs({currentUser: session.user, prrrs})
+}
